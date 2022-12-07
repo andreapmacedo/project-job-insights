72cb04fb9f3474d5cce3b10ed9641530114709a9
andreapmacedo <aa.andremacedo@gmail.com>
2022-12-06 22:14:40 -0300

    feat: Requisito 01.

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,5 +1,6 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
@@ -16,7 +17,9 @@ def read(path: str) -> List[Dict]:
     list
         List of rows as dicts
     """
-    raise NotImplementedError
+    with open(path, encoding='utf8') as archive:
+        data = csv.DictReader(archive, delimiter=',', quotechar='"')
+        return list(data)
 
 
 def get_unique_job_types(path: str) -> List[str]:
@@ -34,7 +37,11 @@ def get_unique_job_types(path: str) -> List[str]:
     list
         List of unique job types
     """
-    raise NotImplementedError
+    data = read(path)
+    unique_jobs = set()
+    for job in data:
+        unique_jobs.add(job['job_type'])
+    return list(unique_jobs)
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
@@ -53,4 +60,3 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
         List of jobs with provided job_type
     """
     raise NotImplementedError
-
