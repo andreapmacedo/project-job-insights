7e61b7637bd03a0e1448549f575388bb9cee5bab
andreapmacedo <aa.andremacedo@gmail.com>
2022-12-14 13:41:37 -0300

    feat: Adiciona solução para o requisito 3.

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,4 +1,5 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
@@ -16,7 +17,12 @@ def get_unique_industries(path: str) -> List[str]:
     list
         List of unique industries
     """
-    raise NotImplementedError
+    get_all_industries = read(path)
+    industries = set()
+    for industry in get_all_industries:
+        if industry["industry"] != '':
+            industries.add(industry["industry"])
+    return industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
