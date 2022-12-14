bf52c4e38e60f766364759bd3da8eca3ff681a8c
andreapmacedo <aa.andremacedo@gmail.com>
2022-12-14 13:56:23 -0300

    feat: Adiciona requisito 5.

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -5,38 +5,12 @@ import csv
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
     with open(path, encoding='utf8') as archive:
         data = csv.DictReader(archive, delimiter=',', quotechar='"')
         return list(data)
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
     data = read(path)
     unique_jobs = set()
     for job in data:
@@ -45,19 +19,5 @@ def get_unique_job_types(path: str) -> List[str]:
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
     filtered_job_type = [job for job in jobs if job["job_type"] == job_type]
     return filtered_job_type
--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,65 +1,27 @@
 from typing import Union, List, Dict
+# from functools import lru_cache
+from src.insights.jobs import read
+import csv
 
+# @lru_cache
+# def read(path: str) -> List[Dict]:
+#     with open(path, encoding='utf8') as archive:
+#         data = csv.DictReader(archive, delimiter=',', quotechar='"')
+#         return list(data)
 
 def get_max_salary(path: str) -> int:
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
     raise NotImplementedError
 
 
 def get_min_salary(path: str) -> int:
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    get_salaries = read(path)
+    return min([
+        int(salaries["min_salary"])
+        for salaries in get_salaries
+        if (salaries["min_salary"]).isdigit()])
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
     raise NotImplementedError
 
 
@@ -67,18 +29,4 @@ def filter_by_salary_range(
     jobs: List[dict],
     salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
     raise NotImplementedError
