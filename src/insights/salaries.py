6ed97bec1339b3f627fb1da7713104f3b23b9ca2
andreapmacedo <aa.andremacedo@gmail.com>
2022-12-14 14:02:09 -0300

    refactory: Refatora solução para os requisitos 5 e 6.

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -4,15 +4,21 @@ from src.insights.jobs import read
 
 
 def get_max_salary(path: str) -> int:
-    raise NotImplementedError
+    get_salaries = read(path)
+    max_salary = max([
+        int(salaries["max_salary"])
+        for salaries in get_salaries
+        if (salaries["max_salary"]).isdigit()])
+    return max_salary
 
 
 def get_min_salary(path: str) -> int:
     get_salaries = read(path)
-    return min([
+    min_salary = min([
         int(salaries["min_salary"])
         for salaries in get_salaries
         if (salaries["min_salary"]).isdigit()])
+    return min_salary
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
